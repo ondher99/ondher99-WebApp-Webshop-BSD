4d1d81dece71b63f098a14f5c76a762c0ee8bba5
HeringhSoma <92676092+HeringhSoma@users.noreply.github.com>
2024-05-26 16:46:15 +0200

    home screen initial

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,32 @@
 import RegistrationForm from "../Registration/Registration";
+import HomeScreen from '../HomeScreen/Home';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <RegistrationForm />
-    </div>
+    <Router>
+      <div>
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link>
+          {/* Add other links */}
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/Registration" element={<RegistrationForm />} />
+          {/* Define other routes */}
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/HomeScreen/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const HomeScreen: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Webshop</h1>
+      <Link to="/login">Login</Link> | <Link to="../Registration">Register</Link>
+      {/* Links or buttons for profile, cart, categories */}
+    </div>
+  );
+};
+
+export default HomeScreen;
