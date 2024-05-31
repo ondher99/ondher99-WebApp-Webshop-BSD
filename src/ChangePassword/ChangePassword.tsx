e8c7aad34eb89b78a200880b7e15157e7014ba8e
HeringhSoma <92676092+HeringhSoma@users.noreply.github.com>
2024-05-31 20:52:54 +0200

    changePassword completed

--- src/ChangePassword/ChangePassword.tsx
+++ src/ChangePassword/ChangePassword.tsx
@@ -1,59 +1,67 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser, getUsers } from '../Profile/UserContext';
 
 const ChangePasswordForm = () => {
+    const navigate = useNavigate();
     const [oldPassword, checkOldPassword] = useState('');
     const [password, newPassword] = useState('');
     const [passwordConfirm, confirmNewPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
-    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const payload = {
         oldPassword: oldPassword.trim(),
         password: password.trim(),
         passwordConfirm: passwordConfirm.trim()
     };
-    console.log(oldPassword)
-    console.log(password)
-    console.log(passwordConfirm)
     try {
-        const response = await changePassword('http://localhost:5000/user/login', payload);
-        console.log(response);   
+        const response = await changePassword(payload);
+        if(response.status == 204){
+            navigate('/')
+        }
         } catch (error) {
-            console.error(error);
+        if (error instanceof Error) {  
+            alert(`${error.message}`);
+            } else {
+            alert('Password change failed. Error unknown');
+            }
         }
     }
-function changePassword(url = "", passwordChangeData = {}) {
+    function ValidatePassword(inputPassword: string) {
+        const regex = /^(?=.*[a-z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        if(!regex.test(inputPassword)){
+          setPasswordError('Invalid password format');
+          return false;
+      }
+    setPasswordError('');
+    return true;
+    }
+    interface passwordChangeData{
+        oldPassword: string;
+        password: string;
+        passwordConfirm: string;
+    }
+      async function changePassword(passwordChangeData: passwordChangeData) {
         const authtoken = localStorage.getItem('accessToken')
         const myHeaders = new Headers({
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + authtoken
         });
         
-        return fetch(url, {
+        const response = await fetch('http://localhost:5000/user/login', {
           method: 'PATCH',
           headers: myHeaders,
           body: JSON.stringify(passwordChangeData)
         })
         
-        .then(response => {
-            console.debug(response.status);
-          if (response.status == 204) {
-            return response.json();
-          } else if (response.status == 403) {
-            throw new Error('Hibás régi jelszó');
-          } else if (response.status == 404) {
-            throw new Error('A felhasználó nem található');
-          } else {
-            throw new Error('A régi és új jelszó nem egyezhet meg');
-          }
-        })
-        .then(response => {
-          console.debug(response);
-          return response;
-        }).catch(error => {
-          console.error(error);
-        });
+        if(response.ok){
+            return response;
+        }else{
+            const errorData = await response.json();
+            throw new Error('Task failed: ' + errorData.message);
+        }
     }
     return(
         <div>
@@ -61,16 +69,17 @@ function changePassword(url = "", passwordChangeData = {}) {
             <form onSubmit={handleSubmit}>
                 <label>
                     Old Password:
-                   <input type="password" name="oldPassword" value={oldPassword} onChange={e => checkOldPassword(e.target.value)} required />
+                   <input type="password" name="oldPassword" value={oldPassword} onChange={e => checkOldPassword(e.target.value)} onBlur={(e) => {ValidatePassword(e.target.value)}} required />
                 </label>
                 <label>
                     New Password:
-                    <input type="password" name="password" value={password} onChange={e => newPassword(e.target.value)} required />
+                    <input type="password" name="password" value={password} onChange={e => newPassword(e.target.value)} onBlur={(e) => {ValidatePassword(e.target.value)}} required />
                 </label>
                 <label>
                     Confirm New Password:
-                    <input type="password" name="confirmPassword" value={passwordConfirm} onChange={e => confirmNewPassword(e.target.value)} required />
+                    <input type="password" name="confirmPassword" value={passwordConfirm} onChange={e => confirmNewPassword(e.target.value)} onBlur={(e) => {ValidatePassword(e.target.value)}}  required />
                 </label>
+                {passwordError && <p style={{color: 'red'}}>{passwordError}</p>}
                 <button type="submit">Confirm new password</button>
             </form>
         </div>
