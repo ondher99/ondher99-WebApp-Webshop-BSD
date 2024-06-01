484dbed69e0ac31c89cdb759fd991ef80b0ec6c9
HeringhSoma <92676092+HeringhSoma@users.noreply.github.com>
2024-06-01 06:43:06 +0200

    cleanup

--- src/Profile/UserContext.tsx
+++ src/Profile/UserContext.tsx
@@ -1,72 +1,4 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-//import getUsers from '../index';
-
-/*export interface User {
-    userId: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    shippingAddress: {
-      name: string;
-      country: string;
-      city: string;
-      street: string;
-      zip: string;
-      phoneNumber: string;
-    };
-    billingAddress: {
-      name: string;
-      country: string;
-      city: string;
-      street: string;
-      zip: string;
-      taxNumber: string;
-    };
-  }
-
-type UserContextType = {
-  user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User | null>>;
-};
-
-const UserContext = createContext<UserContextType | undefined>(undefined);
-
-interface UserProviderProps {
-  children: ReactNode;
-}
-
-export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
-  
-  useEffect(() => {
-    const authtoken = localStorage.getItem('accessToken');  
-    if (!authtoken) {
-      console.error("No access token found");
-      return;
-    }
-    getUsers()
-      .then(response => {
-        setUser(response);
-      })
-      .catch(error => {
-        console.error("Failed to fetch user:", error);
-      });
-  }, []);
-
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
-
-export const useUser = () => {
-  const context = useContext(UserContext);
-  if (context === undefined) {
-    throw new Error('useUser must be used within a UserProvider');
-  }
-  return context;
-};*/
 
 interface User {
     userId: string;
@@ -97,7 +29,7 @@ type UserContextType = {
 };
 
 export function getUsers() {
-  // Retrieve the token from localStorage within the function
+ 
   const authtoken = localStorage.getItem('accessToken');
   
   if (!authtoken) {
@@ -115,7 +47,7 @@ export function getUsers() {
   })
   .then(response => {
     if (response.status === 200) {
-      return response.json(); // if the response is successful, parse the JSON body
+      return response.json();
     } else {
       throw new Error('Something went wrong on the api server!');
     }
@@ -126,7 +58,7 @@ export function getUsers() {
   })
   .catch(error => {
     console.error(error);
-    throw error; // Re-throw the error for further handling
+    throw error; 
   });
 }
 
@@ -140,17 +72,16 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   
   useEffect(() => {
-    const authtoken = localStorage.getItem('accessToken'); // Retrieve the token from localStorage
+    const authtoken = localStorage.getItem('accessToken'); 
     
     if (!authtoken) {
       console.error("No access token found");
-      return; // Early return if there's no token
+      return; 
     }
 
-    // Fetch user data using the Authorization header with the token
     getUsers()
       .then(response => {
-        setUser(response); // Assuming response is of UserType
+        setUser(response); 
       })
       .catch(error => {
         console.error("Failed to fetch user:", error);
